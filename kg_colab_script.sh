35c16af8eee053b9d0627071d5b2c2fff7c76ef4
Harry Pham <83818093+harryypham@users.noreply.github.com>
2024-03-08 10:26:31 +1100

    Update kg_colab_script.sh
    Have to upload kaggle.json manually to current directory

--- kg_colab_script.sh
+++ kg_colab_script.sh
@@ -1,11 +1,17 @@
 # Script to download dataset from Kaggle to Google Colab
+# Have to upload kaggle.json manually to current directory
+#!/bin/bash
 pip install -q kaggle
-echo "from google.colab import files 
-files.upload()" >> script_upload_file.py
-python script_upload_file.py
 mkdir ~/.kaggle
 cp kaggle.json ~/.kaggle/
 chmod 600 ~/.kaggle/kaggle.json
 kaggle datasets download -d $1
 mkdir dataset
-unzip "$1.zip" -d dataset
+IFS='/' read -ra ADDR <<< "$1"
+for ((i=0; i<${#ADDR[@]}; i++ ));
+do
+  if [ $i -eq 1 ]
+  then
+    unzip "${ADDR[$i]}.zip" -d dataset
+  fi
+done
